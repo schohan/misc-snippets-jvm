188d2713a7aa939ad7216e2a7659d606fcfcc2ad
schohan <shailenderchohan@yahoo.com>
2024-09-12 08:43:48 -0400

    removed unused files

--- src/main/groovy/ssc/geo/entity/LocationRepository.groovy
+++
@@ -1,9 +0,0 @@
-package ssc.geo.entity
-
-import org.springframework.data.repository.CrudRepository
-
-/**
- * Created by schohan on 9/4/2015.
- */
-public interface LocationRepository extends CrudRepository<Location, String> {
-}
